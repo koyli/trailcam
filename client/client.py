ee1a6e3912532f38b19f52f64ab1dd26a2461e40
David Lecomber <david@lecomber.net>
2026-04-06 13:30:13 +0100

    add extra time to find device for BLE

--- client/client.py
+++ client/client.py
@@ -17,7 +17,7 @@ async def run(bt_local_id, address):
     print(f"Searching for and connecting to {address}...")
     
     try:
-        async with BleakClient(address, adapter=bt_local_id) as client:
+        async with BleakClient(address, adapter=bt_local_id, timeout=10.0) as client:
             if client.is_connected:
                 print(f"Connected to {address}")
                 
